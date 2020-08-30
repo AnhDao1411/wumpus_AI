bb7493e96363384ec2d4fe85eab43203b47a8ffb
anhdao14112000 <dao.nguyen@f4.intel.edu.vn>
2020-08-31 01:07:11 +0700

    print percept

--- source_2.py
+++ source_2.py
@@ -7,6 +7,7 @@ Wumpus = []
 Oke = []
 Visited = []
 
+clauses = []
 
 def load_level(path):
     file = path
@@ -65,6 +66,7 @@ def Is_Wumpus_dead(pos, size, cave):
     if 'W' not in cave[x][y]:
         return False
     else:
+        print('Shot wumpus successfully at ',x,y)
         cave[x][y] = cave[x][y].replace('W', '')
         if x-1 >= 0:
             if No_Wumpus_Around((x-1, y), size, cave):
@@ -207,28 +209,101 @@ def found_BriSten(pos, size, shot, cave):
                 Oke[x][y + 1] = True
         Pit[x][y+1] += 1
 
+def create_clauses(breeze,stench,pos,size):
+    Prove_Oke(pos)
+    print('New_percept_at: ',pos[0],',',pos[1])
+    x=pos[0]
+    y=pos[1]
+    if not breeze:
+        if (x-1>=0) and (not Visited[x-1][y]):
+            clauses.append(['-P',x-1,y])
+            print('-P[ ',x-1,' ][ ',y,' ]')
+        if (x+1 <= size-1) and (not Visited[x+1][y]):
+            clauses.append(['-P',x+1,y])
+            print('-P[ ',x+1,' ][ ',y,' ]')
+        if (y-1>=0) and (not Visited[x][y-1]):
+            clauses.append(['-P',x,y-1])
+            print('-P[ ',x,' ][ ',y-1,' ]')
+        if (y+1 <= size-1) and (not Visited[x][y+1]):
+            clauses.append(['-P',x,y+1])
+            print('-P[ ',x,' ][ ',y+1,' ]')
+    else:
+        temp = []
+        if (x-1>=0) and (not Visited[x-1][y]):
+            temp.append(['P',x-1,y])
+        if (x+1 <= size-1) and (not Visited[x+1][y]):
+            temp.append(['P',x+1,y])
+        if (y-1>=0) and (not Visited[x][y-1]):
+            temp.append(['P',x,y-1])
+        if (y+1 <= size-1) and (not Visited[x][y+1]):
+            temp.append(['P',x,y+1])
+        clauses.append(temp)
+        for i,ele in enumerate(temp):
+            if i < len(temp)-1:
+                print('P[ ',ele[1],' ][ ',ele[2],' ] OR',end =" ")
+            else:
+                print('P[ ',ele[1],' ][ ',ele[2],' ]')
 
+    if not stench:
+        if (x-1>=0) and (not Visited[x-1][y]):
+            clauses.append(['-W',x-1,y])
+            print('-W[ ',x-1,' ][ ',y,' ]')
+        if (x+1 <= size-1) and (not Visited[x+1][y]):
+            clauses.append(['-W',x+1,y])
+            print('-W[ ',x+1,' ][ ',y,' ]')
+        if (y-1>=0) and (not Visited[x][y-1]):
+            clauses.append(['-W',x,y-1])
+            print('-W[ ',x,' ][ ',y-1,' ]')
+        if (y+1 <= size-1) and (not Visited[x][y+1]):
+            clauses.append(['-W',x,y+1])
+            print('-W[ ',x,' ][ ',y+1,' ]')
+    else:
+        temp = []
+        if (x-1>=0) and (not Visited[x-1][y]):
+            temp.append(['W',x-1,y])
+        if (x+1 <= size-1) and (not Visited[x+1][y]):
+            temp.append(['W',x+1,y])
+        if (y-1>=0) and (not Visited[x][y-1]):
+            temp.append(['W',x,y-1])
+        if (y+1 <= size-1) and (not Visited[x][y+1]):
+            temp.append(['W',x,y+1])
+        clauses.append(temp)
+        for i,ele in enumerate(temp):
+            if i < len(temp)-1:
+                print('W[ ',ele[1],' ][ ',ele[2],' ] OR',end =" ")
+            else:
+                print('W[ ',ele[1],' ][ ',ele[2],' ]')
+                
+    print('-----------------------------------')
+    
+def Prove_Oke(pos):
+    if (['-P',pos[0],pos[1]] in clauses) and (['-W',pos[0],pos[1]] in clauses):
+        print('-P[ ',pos[0],' ][ ',pos[1],' ] AND -W[ ',pos[0],' ][ ',pos[1],' ] -> OKE[ ',pos[0],' ][ ',pos[1],' ]')
+        
 def take_percept(pos, size, shot, cave):
     x = pos[0]
     y = pos[1]
     Oke[x][y] = True
     check_BS = False
-
     if 'B' in cave[x][y] and 'S' in cave[x][y]:
         Breeze[x][y] = True
         Stench[x][y] = True
         found_BriSten(pos, size, shot, cave)
         check_BS = True
+        create_clauses(True,True,pos,size)
     elif 'B' in cave[x][y]:
         Breeze[x][y] = True
         found_Breeze(pos, size)
         check_BS = True
+        create_clauses(True,False,pos,size)
     elif 'S' in cave[x][y]:
         Stench[x][y] = True
         found_Stench(pos, size, shot, cave)
         check_BS = True
+        create_clauses(False,True,pos,size)
     if check_BS:
         return
+    create_clauses(False,False,pos,size)
     # 4 cells around is OK
     # x-1,y
     if x-1 >= 0:
@@ -304,6 +379,8 @@ def Game(path):
     size, cave = load_level(path)
     init(size)
     init_pos = Agent_init_pos(size, cave)
+    clauses.append(['-P',init_pos[0],init_pos[1]])
+    clauses.append(['-W',init_pos[0],init_pos[1]])
     Pick_Gold = []
     Shot_Wumpus = []
     Score = []
@@ -325,47 +402,53 @@ def Game(path):
     return Agent_path, Score, Pick_Gold, Shot_Wumpus
 
 
-def print_percept(size, oke):
-    W, P, V = [], [], []
-
-    for i in range(size):
-        for j in range(size):
-            if Wumpus[i][j] and (i, j) not in Visited:
-                W += [(i, j)]
-            if Pit[i][j]:
-                P += [(i, j)]
-
-    print('Wumpus:', W)
-    print('Pit:', P)
-    print('Oke:', oke)
-
-
 def Wumpus_Agent(init_pos, size, cave, Pick_Gold, Shot_Wumpus, Score):
     Agent_path = []
     Agent_pos = init_pos
+    Visited[init_pos[0]][init_pos[1]] = True
     while True:
         score_room = 0
         take_percept(Agent_pos, size, Shot_Wumpus, cave)
+        x = Agent_pos[0]
+        y = Agent_pos[1]
+        if 'G' in cave[x][y]:
+            Gold[x][y] = True
+            Pick_Gold.append(new_pos)
+            score_room += 100
+            cave[x][y] = cave[x][y].replace('G', '')
+        if x-1 >= 0:
+            if Wumpus[x-1][y] > 1:
+                score_room -= 100
+                Shot_Wumpus.append(((x, y), (x-1, y)))
+                Is_Wumpus_dead((x-1, y), size, cave)
+        if x+1 <= size-1:
+            if Wumpus[x+1][y] > 1:
+                score_room -= 100
+                Shot_Wumpus.append(((x, y), (x+1, y)))
+                Is_Wumpus_dead((x+1, y), size, cave)
+        if y-1 >= 0:
+            if Wumpus[x][y-1] > 1:
+                score_room -= 100
+                Shot_Wumpus.append(((x, y), (x, y-1)))
+                Is_Wumpus_dead((x, y-1), size, cave)
+        if y+1 <= size-1:
+            if Wumpus[x][y+1] > 1:
+                score_room -= 100
+                Shot_Wumpus.append(((x, y), (x, y+1)))
+                Is_Wumpus_dead((x, y+1), size, cave)
+
         Oke_pos = find_Oke_pos(size)
         if not Oke_pos:
             return Agent_path, 'no_room'
         Oke_pos = sorted(Oke_pos, key=lambda x: distance(x, Agent_pos))
         new_pos = Oke_pos.pop(0)
         score_room -= 10
-        x = new_pos[0]
-        y = new_pos[1]
-        Visited[x][y] = True
-        if 'G' in cave[x][y]:
-            Gold[x][y] = True
-            Pick_Gold.append(new_pos)
-            score_room += 100
-            cave[x][y] = cave[x][y].replace('G', '')
+        Visited[new_pos[0]][new_pos[1]] = True
         path = BFS(Agent_pos, new_pos, size, cave)
         for i in range(1, len(path)):
             Agent_path.append(path[i])
         Agent_pos = new_pos
         Score.append(score_room)
 
-        print_percept(size, Oke_pos)
 
-Game('./Data/map1.txt')
+Game('./Data/map5.txt')
